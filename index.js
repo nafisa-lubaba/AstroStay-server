756934d893f50d91b676be7a8b24f3d969105047
Acer <nafisalubaba71@gmail.com>
2024-11-05 22:11:34 +0600

    added-rooms

--- index.js
+++ index.js
@@ -1,15 +1,73 @@
+// const express = require('express');
+// const cors = require('cors');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config();
+
+// const port = process.env.PORT || 5000;
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// console.log(process.env.DB_PASS)
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrsyrpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+// app.get('/', (req, res) => {
+//     res.send('website is running')
+//   })
+//   app.listen(port, () => {
+//     console.log(`website server is running on port:${port}`)
+//   })
+
+
+
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const jwt = require('jsonwebtoken')
+// const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 6000;
 const app = express();
-app.use(cors());
-app.use(express.json());
-console.log(process.env.DB_PASS)
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
 
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+ 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrsyrpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,22 +82,34 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const roomsCollection = client.db('roomDB').collection('rooms')
+    const reviewCollection = client.db('roomDB').collection('review')
+    const bookingCollection = client.db('roomDB').collection('booking')
+    //jwt
+    
+    // Clear token on logout
+    app.get('/rooms', async (req, res) => {
+      const cursor = roomsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('website is running')
-  })
-  app.listen(port, () => {
-    console.log(`website server is running on port:${port}`)
-  })
+  res.send('website is running')
+})
+app.listen(port, () => {
+  console.log(`website server is running on port:${port}`)
+})
