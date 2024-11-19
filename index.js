7732b11948ded164b2032566717d3aaa1d210605
Acer <nafisalubaba71@gmail.com>
2024-11-19 20:34:26 +0600

    deploy

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
+     'http://localhost:5175',
+     'https://astrostay-server-production.up.railway.app',
 
   ],
   credentials: true,
